717ed98c5d5ee02d3d0405844db75bafc0fca86d
Ramir-o <zhouba1516@gmail.com>
2026-06-01 20:52:09 +0800

    Fix formatting of skip_sha256_check output

--- upload.sh
+++ upload.sh
@@ -381,7 +381,7 @@ init_var() {
     echo -e "${INFO} allow_updates:      [ ${allow_updates} ]"
     echo -e "${INFO} remove_artifacts:   [ ${remove_artifacts} ]"
     echo -e "${INFO} replaces_artifacts: [ ${replaces_artifacts} ]"
-    echo -e "${INFO} skip_sha256_check: [ ${skip_sha256_check} ]"
+    echo -e "${INFO} skip_sha256_check:  [ ${skip_sha256_check} ]"
     echo -e "${INFO} upload_timeout:     [ ${timeout_display} ]"
     echo -e "${INFO} make_latest:        [ ${make_latest} ]"
     echo -e "${INFO} prerelease:         [ ${prerelease} ]"
